b25ba76b094d8ed969b145ed3ba276fc50b68bf8
miyanda2 <myke1332@gmail.com>
2021-12-12 10:09:03 +0100

    ajala class

--- src/ajala.php
+++ src/ajala.php
@@ -1,9 +1,15 @@
 <?php 
 namespace httpTransport;
 class Ajala
-{
+{   
+    
+    public function __construct()
+    {
+        //
+    }
+
     public static function ajala()
     {
-        //return '';
+        //
     }
 }
